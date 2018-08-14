ebf54371e36f0e6e0a0f789d62f2121d8d582cf3
Gosuke Miyashita <gosukenator@gmail.com>
2018-08-14 15:08:42 +0900

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,62 @@
-var publicDir = __dirname + '/public';
-module.exports = {
-    mode: 'development',
-    entry: [
-        './src/index.js'
-    ],
-    output: {
-        path: publicDir,
-        publicPath: '/',
-        filename: 'bundle.js'
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+ 
+const publicDir = path.join(__dirname, '/public');
+module.exports = [
+    {
+        mode: 'development',
+        devServer: {
+            historyApiFallback: true,
+            contentBase: publicDir,
+        },
     },
-    module: {
-        rules: [{
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['react', 'es2015']
-            }
-        }]
+    {
+        mode: 'development',
+        entry: [
+            './src/index.js',
+        ],
+        output: {
+            path: publicDir,
+            publicPath: '/',
+            filename: 'bundle.js',
+        },
+        module: {
+            rules: [{
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015'],
+                },
+            }],
+        },
+        resolve: {
+            extensions: ['.js', '.jsx'],
+        },
     },
-    resolve: {
-        extensions: ['.js', '.jsx']
+    {
+        mode: 'development',
+        entry: {
+            style: './stylesheets/index.scss',
+        },
+        output: {
+            path: publicDir,
+            publicPath: '/',
+            filename: 'bundle.css',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }),
+                },
+                {
+                    test: /\.scss$/,
+                    loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' }),
+                },
+            ],
+        },
+        plugins: [
+            new ExtractTextPlugin('bundle.css'),
+        ],
     },
-    devServer: {
-        historyApiFallback: true,
-        contentBase: publicDir
-    }
-};
-
+];
