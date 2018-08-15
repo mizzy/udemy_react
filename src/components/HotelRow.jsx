417b72807f1da1e83c705c9a028ef8e545a22ea9
Gosuke Miyashita <gosukenator@gmail.com>
2018-08-15 18:26:33 +0900

    commit

--- src/components/HotelRow.jsx
+++ src/components/HotelRow.jsx
@@ -3,14 +3,24 @@ import PropTypes from 'prop-types';
 
 const HotelRow = ({ hotel }) => (
     <tr>
+      <td><img src={hotel.thumbUrl} alt={hotel.name} /></td>
       <td><a href={hotel.url} target="_blank">{hotel.name}</a></td>
+      <td>{hotel.price}</td>
+      <td>{hotel.reviewAverage}</td>
+      <td>{hotel.reviewCount}</td>
+      <td>{hotel.distance}</td>
     </tr>
 );
 
 HotelRow.PropTyeps = {
     hotels: PropTypes.shape({
         name: PropTypes.string,
-        url: PropTypes.string,        
+        url: PropTypes.string,
+        thumbUrl: PropTypes.string,
+        price: PropTypes.string,
+        reviewAverage: PropTypes.number,
+        reviewCount: PropTypes.number,
+        distance: PropTypes.number,
     }).isRequired,
 };
 
