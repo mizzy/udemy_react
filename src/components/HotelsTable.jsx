7b447bec9d8a4894c984ec7661f6d7f8b8bd0cb2
Gosuke Miyashita <gosukenator@gmail.com>
2018-08-20 14:54:18 +0900

    commit

--- src/components/HotelsTable.jsx
+++ src/components/HotelsTable.jsx
@@ -2,16 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import HotelRow from './HotelRow';
+import HotelsClickableTh from './HotelsClickableTh';
 
-const HotelsTable = ({ hotels }) => (
+const HotelsTable = ({ hotels, sortKey, onSort }) => (
     <table>
       <tbody>
         <tr>
           <th>画像</th>
           <th>ホテル名</th>
-          <th className="hotel-price-column">値段</th>
-          <th>レビュー</th>
-          <th>レビュー件数</th>
+          <HotelsClickableTh
+            label="値段"
+            sortKey="price"
+            isSelected={sortKey === 'price'}
+            onSort={key => onSort(key)}
+            />
+          <HotelsClickableTh
+            label="レビュー"
+            sortKey="reviewAverage"
+            isSelected={sortKey === 'reviewAverage'}
+            onSort={key => onSort(key)}
+            />
+           <th>レビュー件数</th>
           <th>距離</th>
         </tr>
         {hotels.map(hotel => (<HotelRow key={hotel.id} hotel={hotel} />))}
@@ -21,6 +32,8 @@ const HotelsTable = ({ hotels }) => (
 
 HotelsTable.PropTyeps = {
     hotels: PropTypes.arrayOf(PropTypes.any),
+    sortKey: PropTypes.string.isRequired,
+    onSort: PropTypes.func.isRequired,
 };
 
 HotelsTable.defaultProps = {
